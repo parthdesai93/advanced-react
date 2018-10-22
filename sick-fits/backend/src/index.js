b0624c13498ff9082e5a2841ef28a838027f4501
Parth Desai <psdesai.93@gmail.com>
2018-10-22 17:28:15 -0400

    setup graphql backend

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,1 +1,15 @@
-// let's go!
+require('dotenv').config({path: '.env'});
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    }
+  },
+  res => console.log(`server is now running on ${res.port}`)
+);
